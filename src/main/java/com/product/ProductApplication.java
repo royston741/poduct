4418cf01e4e1a617525023617f0c77d25fc56ad5
Royston Rodrigues <roy71ston@gmail.com>
2023-10-30 17:00:51 +0530

    new changes

--- src/main/java/com/product/ProductApplication.java
+++ src/main/java/com/product/ProductApplication.java
@@ -8,7 +8,7 @@ public class ProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
-		System.out.println("Server is running.....");
+		System.out.println("Server is running ....");
 	}
 
 }
